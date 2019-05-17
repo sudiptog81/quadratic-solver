90ad3de30df7fa87fab46bb551d92b723286255c
Sudipto Ghosh <sudipto@ghosh.pro>
2019-05-17 07:39:05 +0530

    fix renaming

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-unused-vars */
 import Solver from "./classes/Solver";
 import MatrixSolver from "./classes/MatrixSolver";
-import getVersion from "./methods/GetVersion";
-import quadSolver from "./methods/QuadSolver";
-import completeSquare from "./methods/CompleteSquare";
-import quadSolverCitardauq from "./methods/QuadSolverCitardauq";
+import getVersion from "./methods/getVersion";
+import quadSolver from "./methods/quadSolver";
+import completeSquare from "./methods/completeSquare";
+import quadSolverCitardauq from "./methods/quadSolverCitardauq";
 
 export {
   Solver, MatrixSolver, getVersion, quadSolver, quadSolverCitardauq, completeSquare
